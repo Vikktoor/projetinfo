e3cab07dd21da8bf4d515cee2b0c61f0da47d2da
Tom-Insa <eyertom@gmail.com>
2024-11-04 12:59:39 +0100

    mouvement.pas
    unite mouvement

--- mouvement.pas
+++ mouvement.pas
@@ -2,46 +2,76 @@ unit mouvement;
 
 
 interface
-uses types,interaction,crt,math;
+uses crt,math,types; 
+
 
 	
+var 
+touche:char;
 
-procedure affich(Tank1,Tank2:tank); {actualiser la position du tank}
+procedure InitialiserTank(var j: joueur; initX, initY: Integer; initAngle: Real);
+procedure affich(j1,j2:joueur);
+procedure avancer(var j:joueur;sens:integer);
+procedure TournerGauche(var j:joueur);
+procedure TournerDroite(var j:joueur);
 
 
-procedure avancer(var T:tank;angledeg:real; newx,newy: integer);
+implementation 
 
+procedure InitialiserTank(var j:joueur; initX, initY: Integer; initAngle: Real);
+begin
+  j.t.x := initX;
+  j.t.y := initY;
+  j.t.posx:= initX;
+  j.t.posy:= initY;
+  j.t.direction:= initAngle;
+end;
 
-
-implementation
-
-procedure affich(Tank1,Tank2:tank); {lactualiser la position du tank}
+procedure affich(j1,j2:joueur); {actualiser la position du tank}
 begin
 	ClrScr;
-	GotoXY(Tank1.x,Tank1.y);
+	GotoXY(j1.t.x,j1.t.y);
 	write('x');
-	GotoXY(Tank2.x,Tank2.y);
+	GotoXY(j2.t.x,j2.t.y);
 	write('o');
 end;
 
-
-procedure avancer(var T:tank;angledeg:real; newx,newy: integer);
-var anglerad:real;
+procedure avancer(var j:joueur;sens:integer);
+var anglerad:real;newx,newy:real;
 begin
-anglerad:=angledeg*Pi/180;
-newx:=round(cos(anglerad));
-newy:=round(sin(anglerad));
-	T.x:=T.x+newx;
-	T.y:=T.y+newy;
-	
-	if T.x>ScreenWidth  {ne pas sortir de l'écran}
-	then T.x:=ScreenWidth;
-	if T.y>ScreenHeight
-	then T.y:=ScreenHeight;
+anglerad:=j.t.direction*Pi/180;
+
+newx:= j.t.coefVit * cos(anglerad)*sens;
+newy:= j.t.coefVit * sin(anglerad);
+
+	j.t.posx:=j.t.posx+newx;
+	j.t.posy:=j.t.posy+newy;
+
+j.t.x := Trunc(j.t.posx);
+j.t.y := Trunc(j.t.posy);
+
+  if j.t.x < 1 
+  then j.t.x := 1;
+  if j.t.y < 1 
+  then j.t.y := 1;
+	if j.t.x>ScreenWidth  {ne pas sortir de l'écran}
+	then j.t.x:=ScreenWidth;
+	if j.t.y>ScreenHeight
+	then j.t.y:=ScreenHeight;
 end;
 
+procedure TournerGauche(var j: joueur);
+begin
+  j.t.direction := j.t.direction - 22.5;
+  if j.t.direction < 0 then
+    j.t.direction := j.t.direction + 360;
+end;
 
-BEGIN
-	
-	
-END.
+procedure TournerDroite(var j: joueur);
+begin
+  j.t.direction := j.t.direction + 22.5;
+  if j.t.direction >= 360 then
+    j.t.direction := j.t.direction - 360;
+end;
+
+end.
