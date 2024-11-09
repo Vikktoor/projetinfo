b34be7d8cf79d5fbeb5c1a72787938de62ace9fd
Tom-Insa <eyertom@gmail.com>
2024-11-09 20:53:36 +0100

    Update types.pas
    creation intialisation des cartes et des tableaux des 3 cartes + tableau regroupant chaque tableau d'obstacle

--- types.pas
+++ types.pas
@@ -18,13 +18,14 @@ Type tank = record
 	Munitions:Tabmunition;
 	end;
 	
-type obstacle = record
-	x,y:integer;
-	end;
-	
+
+
+type tobst= array[1..12,1..8] of string;
+type listecarte=array[1..3] of tobst;
+
 type carte = record
 	Largeur,hauteur: integer;
-	Tobstacles: array of array of obstacle;
+	Tob: tobst
 	end;
 	
 type joueur=record
@@ -38,6 +39,12 @@ type Ttank = array [1..3] of tank;
 procedure iniT(var T:Ttank);
 
 procedure iniScore(var j1,j2:joueur);
+
+procedure inicarte1(var car:tobst);
+procedure inicarte2(var car:tobst);
+procedure inicarte3(var car:tobst);
+
+procedure inicartes(car1,car2,car3:tobst;var tabcar:listecarte);
 
 implementation
 
@@ -65,6 +72,105 @@ begin
 	j2.score:=0;       //initialisation des scores
 end;
 
+procedure inicarte1(var car:tobst);// on initialise les obstacles des cartes
+
+begin
+	car[1][1] := 'x';
+	car[2][1] := 'x';
+	car[12][1] := 'x';
+	car[6][2] := 'x';
+	car[7][2] := 'x';
+	car[2][4] := 'x';
+	car[11][4] := 'x';
+	car[2][5] := 'x';
+	car[11][5] := 'x';
+	car[4][6] := 'x';
+	car[5][6] := 'x';
+	car[8][6] := 'x';
+	car[9][6] := 'x';
+	car[1][7] := 'x';
+	car[6][7] := 'x';
+	car[7][7] := 'x';
+	car[12][7] := 'x';
+	car[1][8] := 'x';
+	car[2][8] := 'x';
+	car[12][8] := 'x';
+		
+end;
+
+procedure inicarte2(var car:tobst);
+
+begin
+	car[2][2] := 'x';
+	car[3][2] := 'x';
+	car[6][2] := 'x';
+	car[11][2] := 'x';
+	car[3][3] := 'x';
+	car[4][3] := 'x';
+	car[5][3] := 'x';
+	car[6][3] := 'x';
+	car[10][3] := 'x';
+	car[11][3] := 'x';
+	car[10][4] := 'x';
+	car[2][5] := 'x';
+	car[5][5] := 'x';
+	car[6][5] := 'x';
+	car[7][5] := 'x';
+	car[8][5] := 'x';
+	car[2][6] := 'x';
+	car[8][6] := 'x';
+	car[2][7] := 'x';
+	car[3][7] := 'x';
+	car[11][7] := 'x';
+	car[12][7] := 'x';
+	car[11][8] := 'x';
+	car[12][8] := 'x';
+end;
+
+procedure inicarte3(var car:tobst);
+
+begin
+	car[6][1] := 'x';
+	car[7][1] := 'x';
+	car[10][1] := 'x';
+	car[2][2] := 'x';
+	car[3][2] := 'x';
+	car[4][2] := 'x';
+	car[10][2] := 'x';
+	car[2][3] := 'x';
+	car[3][3] := 'x';
+	car[7][3] := 'x';
+	car[6][4] := 'x';
+	car[7][4] := 'x';
+	car[10][4] := 'x';
+	car[11][4] := 'x';
+	car[2][5] := 'x';
+	car[3][5] := 'x';
+	car[4][5] := 'x';
+	car[5][5] := 'x';
+	car[6][5] := 'x';
+	car[1][7] := 'x';
+	car[2][7] := 'x';
+	car[5][7] := 'x';
+	car[6][7] := 'x';
+	car[7][7] := 'x';
+	car[8][7] := 'x';
+	car[9][7] := 'x';
+	car[10][7] := 'x';
+	car[1][8] := 'x';
+end;
+
+procedure inicartes(car1,car2,car3:tobst;var tabcar:listecarte);
+
+begin
+	inicarte1(car1);
+	inicarte2(car2);
+	inicarte3(car3);
+	tabcar[1]:=car1;
+	tabcar[2]:=car2;
+	tabcar[3]:=car3;
+end;
+end.
 
 
 end.
