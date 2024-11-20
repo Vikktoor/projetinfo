b8d526c239ec537c2cc6cf04a447016aebaf16a7
Tom-Insa <eyertom@gmail.com>
2024-11-20 15:33:49 +0100

    Update types.pas
    modif carte obst

--- types.pas
+++ types.pas
@@ -2,6 +2,10 @@ unit types;
 
 interface
 
+uses SDL2, SDL2_image;
+
+var  SDL_Rect: TSDL_Rect;
+
 type Mun=record
 	nbRebonds,Vx,Vy,x,y:integer;
 	direction:Real;
@@ -22,7 +26,7 @@ Type tank = record
 	
 
 
-type tobst= array[1..12,1..8] of char;
+type tobst= array[1..25] of TSDL_Rect;
 type listecarte=array[1..3] of tobst;
 
 type carte = record
@@ -44,8 +48,7 @@ procedure iniScore(var j1,j2:joueur);
 
 procedure iniMun(j:joueur);
 procedure iniobst1(var car:tobst);
-procedure iniobst2(var car:tobst);
-procedure iniobst3(var car:tobst);
+
 
 procedure inicartes(car1,car2,car3:tobst;var tabcar:listecarte);
 
@@ -69,7 +72,7 @@ BEGIN
 END;
 
 procedure iniScore(var j1,j2:joueur);
-
+ 
 begin
 	j1.score:=0;
 	j2.score:=0;       //initialisation des scores
@@ -85,103 +88,76 @@ end;
 
 
 procedure iniobst1(var car:tobst);// on initialise les obstacles des cartes
-
+var i:integer;
 begin
-	car[1][1] := 'x';
-	car[2][1] := 'x';
-	car[12][1] := 'x';
-	car[6][2] := 'x';
-	car[7][2] := 'x';
-	car[2][4] := 'x';
-	car[11][4] := 'x';
-	car[2][5] := 'x';
-	car[11][5] := 'x';
-	car[4][6] := 'x';
-	car[5][6] := 'x';
-	car[8][6] := 'x';
-	car[9][6] := 'x';
-	car[1][7] := 'x';
-	car[6][7] := 'x';
-	car[7][7] := 'x';
-	car[12][7] := 'x';
-	car[1][8] := 'x';
-	car[2][8] := 'x';
-	car[12][8] := 'x';
+	for i:=1 to 24 do
+		begin
+			car[i].w := 100;
+			car[i].h := 100;
+		end;
+	car[1]:=SDL_Rect(0,0,100,100);
+	car[1].x:=0;
+	car[1].y:=0;
+	car[2].x:=100;
+	car[2].y:=0;
+	car[3].x:=1100;
+	car[3].y:=0;
+	car[4].x:=500;
+	car[4].y:=100;
+	car[5].x:=600;
+	car[5].y:=100;
+	car[6].x:=400;
+	car[6].y:=200;
+	car[7].x:=500;
+	car[7].y:=200;
+	car[8].x:=700;
+	car[8].y:=200;
+	car[9].x:=800;
+	car[9].y:=200;
+	car[10].x:=100;
+	car[10].y:=300;
+	car[11].x:=100;
+	car[11].y:=400;
+	car[12].x:=1000;
+	car[12].y:=300;
+	car[13].x:=1000;
+	car[13].y:=400;
+	car[14].x:=300;
+	car[14].y:=500;
+	car[15].x:=400;
+	car[15].y:=500;
+	car[16].x:=500;
+	car[16].y:=600;
+	car[17].x:=600;
+	car[17].y:=600;
+	car[18].x:=700;
+	car[18].y:=500;
+	car[19].x:=800;
+	car[19].y:=500;
+	car[20].x:=0;
+	car[20].y:=600;
+	car[21].x:=0;
+	car[21].y:=700;
+	car[22].x:=100;
+	car[22].y:=700;
+	car[23].x:=1100;
+	car[23].y:=600; 
+	car[24].x:=1100;
+	car[24].y:=700;
+	
 		
 end;
 
-procedure iniobst2(var car:tobst);
 
-begin
-	car[2][2] := 'x';
-	car[3][2] := 'x';
-	car[6][2] := 'x';
-	car[11][2] := 'x';
-	car[3][3] := 'x';
-	car[4][3] := 'x';
-	car[5][3] := 'x';
-	car[6][3] := 'x';
-	car[10][3] := 'x';
-	car[11][3] := 'x';
-	car[10][4] := 'x';
-	car[2][5] := 'x';
-	car[5][5] := 'x';
-	car[6][5] := 'x';
-	car[7][5] := 'x';
-	car[8][5] := 'x';
-	car[2][6] := 'x';
-	car[8][6] := 'x';
-	car[2][7] := 'x';
-	car[3][7] := 'x';
-	car[11][7] := 'x';
-	car[12][7] := 'x';
-	car[11][8] := 'x';
-	car[12][8] := 'x';
-end;
 
-procedure iniobst3(var car:tobst);
-
-begin
-	car[6][1] := 'x';
-	car[7][1] := 'x';
-	car[10][1] := 'x';
-	car[2][2] := 'x';
-	car[3][2] := 'x';
-	car[4][2] := 'x';
-	car[10][2] := 'x';
-	car[2][3] := 'x';
-	car[3][3] := 'x';
-	car[7][3] := 'x';
-	car[6][4] := 'x';
-	car[7][4] := 'x';
-	car[10][4] := 'x';
-	car[11][4] := 'x';
-	car[2][5] := 'x';
-	car[3][5] := 'x';
-	car[4][5] := 'x';
-	car[5][5] := 'x';
-	car[6][5] := 'x';
-	car[1][7] := 'x';
-	car[2][7] := 'x';
-	car[5][7] := 'x';
-	car[6][7] := 'x';
-	car[7][7] := 'x';
-	car[8][7] := 'x';
-	car[9][7] := 'x';
-	car[10][7] := 'x';
-	car[1][8] := 'x';
-end;
 
 procedure inicartes(car1,car2,car3:tobst;var tabcar:listecarte);
 
 begin
 	iniobst1(car1);
-	iniobst2(car2);
-	iniobst3(car3);
+	
 	tabcar[1]:=car1;
 	tabcar[2]:=car2;
 	tabcar[3]:=car3;
 end;
 end.
-
-
