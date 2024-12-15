8360c46d082ca77b5373d41ffce4009da52cf586
Vikktoor <victor.hrab@outlook.fr>
2024-12-15 11:37:24 +0100

    Update menuTerm.pas

--- menuTerm.pas
+++ menuTerm.pas
@@ -23,12 +23,15 @@ begin
 	writeln('3: carte sable');
 	readln(n);
 	writeln('');
+			
+	//Permet d'avoir obligatoirement une réponse entre 1 et 3
 	while (n<1) or (n>3) do
 		begin
 			writeln('Veuillez entrer un nombre compris entre 1 et 3');
 			readln(n);
 		end;
-
+		
+	//Affichage carte 1
 	if n=1 then
 		begin
 			affCarte1;
@@ -36,14 +39,18 @@ begin
 			writeln('1: oui');
 			writeln('2: non');
 			readln(choix);
+			//Permet d'avoir obligatoirement 1 ou 2
 			while (choix<1) or (choix>2) do
 				begin
 					writeln('Veuillez entrer 1 ou 2');
 					readln(choix);
 				end;
+			//Début de la partie sur la carte 1	
 			if choix = 1 then
 				jouercarte1(j1,j2);
 		end;
+	
+	//Affichage carte 2	
 	if n=2 then
 		begin
 			affCarte2;
@@ -51,14 +58,18 @@ begin
 			writeln('1: oui');
 			writeln('2: non');
 			readln(choix);
+			//Permet d'avoir obligatoirement 1 ou 2
 			while (choix<1) or (choix>2) do
 				begin
 					writeln('Veuillez entrer 1 ou 2');
 					readln(choix);
 				end;
+			//Debut de la partie sur la carte 2	
 			if choix = 1 then
 				jouercarte2(j1,j2);
 		end;
+		
+	//Affichage carte 3	
 	if n=3 then
 		begin
 			affCarte3;
@@ -66,14 +77,16 @@ begin
 			writeln('1: oui');
 			writeln('2: non');
 			readln(choix);
+			//Permet d'avoir obligatoirement 1 ou 2
 			while (choix<1) or (choix>2) do
 				begin
 					writeln('Veuillez entrer 1 ou 2');
 					readln(choix);
 				end;
+			//Debut de la partie sur la carte 3	
 			if choix = 1 then
 				jouercarte3(j1,j2);
 		end;
-end;
+	end;
 
 end.
